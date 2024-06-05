5a293e4b9c40b7eaeb559bd79c4460e034873905
patelhet04 <hetpatel.hp4199@gmail.com>
2024-06-04 23:41:48 -0400

    Created POST request

--- main.go
+++ main.go
@@ -16,10 +16,28 @@ type Todo struct {
 func main() {
 	fmt.Println("Hello world")
 
+	todos := []Todo{}
+
 	app := fiber.New()
 	// To fetch todo items
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(200).JSON(fiber.Map{"name": "John"})
+	})
+
+	app.Post("/api/todo", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+		// In Go, whenver using a pointer always check for errors
+		// BodyParser binds the request body to the struct
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+		if todo.Body == "" {
+			c.Status(400).JSON(fiber.Map{"msg": "Body cannot be empty"})
+		}
+
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+		return c.Status(201).JSON(todo)
 	})
 
 	log.Fatal(app.Listen(":4000"))
