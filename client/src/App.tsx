fe6696cbca7e5f5a3b91349c3907561bf4d521df
patelhet04 <hetpatel.hp4199@gmail.com>
2024-06-09 20:35:01 -0400

    Initial UI and tankstack query setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,19 @@
-import { Button } from "@chakra-ui/react";
-
+import { Button, Container, Stack } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+export const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:4000/api" : "/api";
 function App() {
   return (
     <>
-      <Button>Hello</Button>
+      <Stack>
+        <Navbar />
+        <Container>
+          <TodoForm />
+          <TodoList />
+        </Container>
+      </Stack>
     </>
   );
 }
